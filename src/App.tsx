dc367fbe2ad6dbb914412cbb1c6847c2c28318b4
Rustin <rustinchi.alpha@gmail.com>
2024-08-27 18:56:18 +0800

    🐛 fix: baseUrl

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,12 @@ function App() {
     injectHibitIdTonConnect(CHAIN.TESTNET)
 
     // init tonconnect sdk 
+    const baseUrl = new URL(import.meta.env.BASE_URL, location.origin)
     const connector = new TonConnect({
-      walletsListSource: `${location.origin}/wallets.json`
+      walletsListSource: `${baseUrl}wallets.json`,
     })
     const client: TonConnectUI = (window as any).tonCache || new TonConnectUI({
-      manifestUrl: `${location.origin}/tonconnect-manifest.json`,
+      manifestUrl: `${baseUrl}tonconnect-manifest.json`,
       connector,
     });
     (window as any).tonCache = client
@@ -28,7 +29,7 @@ function App() {
   return (
     <div className="container p-6 mx-auto">
       <h1 className="flex justify-center items-center gap-2 font-bold text-xl">
-        <img src="/logo.png" alt="logo" className="size-8" />
+        <img src={import.meta.env.BASE_URL + 'logo.png'} alt="logo" className="size-8" />
         <span>Hibit ID TonConnect Example</span>
       </h1>
       <main className="mt-8 flex flex-col gap-6">
