03bed34468474dea0e8863a37bddaa406b9892a6
Rustin <rustinchi.alpha@gmail.com>
2024-09-28 10:00:10 +0800

    🐛 fix: icrc29 polling

--- src/pages/ICRC/Icrc29Section.tsx
+++ src/pages/ICRC/Icrc29Section.tsx
@@ -7,26 +7,39 @@ import { IcrcContext } from "./context";
 const Icrc29Section: FC = () => {
   const [request, setRequest] = useState<any | null>(null)
   const [response, setResponse] = useState<any | null>(null)
-  const { iframeWindow, getRequestId } = useContext(IcrcContext)
+  const [polling, setPolling] = useState(false)
+  const { iframeWindow, getRequestId, setIcrc29Ready } = useContext(IcrcContext)
   const reqIdRef = useRef(0)
+  const intervalRef = useRef<NodeJS.Timer | null>(null)
 
   const handleMessage = useCallback((event: MessageEvent) => {
     try {
       const response = parseJsonRpcResponse<Icrc29StatusResult>(event.data, reqIdRef.current)
+      if (!response) {
+        return
+      }
       setResponse(response)
+      clearInterval(intervalRef.current!)
+      setPolling(false)
+      if (response?.result === 'ready') {
+        setIcrc29Ready(true)
+      }
     } catch (e) {
       setResponse(e)
     }
-  }, [])
+  }, [setIcrc29Ready])
 
-  const sendMessage = () => {
-    reqIdRef.current = getRequestId()
-    const req = buildJsonRpcRequest<Icrc29StatusRequest>(reqIdRef.current, IcrcMethods.ICRC29_STATUS, undefined)
-    setRequest(req)
-    iframeWindow?.postMessage(
-      req,
-      '*',
-    )
+  const startPolling = () => {
+    setPolling(true)
+    intervalRef.current = setInterval(() => {
+      reqIdRef.current = getRequestId()
+      const req = buildJsonRpcRequest<Icrc29StatusRequest>(reqIdRef.current, IcrcMethods.ICRC29_STATUS, undefined)
+      setRequest(req)
+      iframeWindow?.postMessage(
+        req,
+        '*',
+      )
+    }, 1000)
   }
 
   useEffect(() => {
@@ -42,9 +55,10 @@ const Icrc29Section: FC = () => {
         <div className="flex flex-col gap-2">
           <button
             className="btn btn-primary"
-            onClick={sendMessage}
+            onClick={startPolling}
+            disabled={polling}
           >
-            Send message
+            Start polling
           </button>
         </div>
         <div className="divider divider-horizontal"></div>
